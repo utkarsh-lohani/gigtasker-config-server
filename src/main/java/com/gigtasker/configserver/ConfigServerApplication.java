97cd281f8f03c7b59ed3b00177e1f84580005062
Utkarsh Lohani <utkarshlohani88380@gmail.com>
2025-11-06 16:41:47 +0530

    Fixed Package Name

--- src/main/java/com/gigtasker/configserver/ConfigServerApplication.java
+++ src/main/java/com/gigtasker/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.gigtasker.config_server;
+package com.gigtasker.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
