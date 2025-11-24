50e46afb36b1c081704f9f30de137b15ce637c1c
Utkarsh Lohani <utkarshlohani88380@gmail.com>
2025-11-24 14:31:37 +0530

    fix: make main method non-public for Java 25 compatibility
    Java 25 enforces stricter requirements around entry point method visibility.
    Changed the application main method from public to package-private and added
    a private constructor to prevent instantiation.

--- src/main/java/com/gigtasker/configserver/ConfigServerApplication.java
+++ src/main/java/com/gigtasker/configserver/ConfigServerApplication.java
@@ -8,7 +8,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
+    private ConfigServerApplication() {}
+
+	static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
